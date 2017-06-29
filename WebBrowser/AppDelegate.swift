4cbcb8360dc93b16609fcc6855066941157bee1a
Daniel Solodimedina <dmdanimedina@gmail.com>
2017-06-29 15:13:34 -0400

    commit base

--- WebBrowser/AppDelegate.swift
+++ WebBrowser/AppDelegate.swift
@@ -7,17 +7,86 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 10.0, *){
+            let center = UNUserNotificationCenter.current()
+                center.delegate = self
+            center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler:{(grant, error) in
+                if error == nil {
+                    if grant {
+                        application.registerForRemoteNotifications()
+                    } else {
+                        // user didnt grant
+                    }
+                } else {
+                    print("Error: ", error as Any )
+                }
+            })
+        }else {
+                // fallback on earlier versions
+            let notificationSettings = UIUserNotificationSettings( types: [.alert, .badge, .sound], categories: nil)
+                application.registerUserNotificationSettings(notificationSettings)
+        }
         return true
     }
+    
+    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings){
+        if notificationSettings.types != .none{
+            application.registerForRemoteNotifications()
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
+        let tokenString = deviceToken.reduce("",{$0 + String(format:"%02X", $1)})
+        
+        print("token :", tokenString)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController: UIViewController = storyboard
+            .instantiateViewController(withIdentifier: "Main") as! ViewController
+        UserDefaults.standard.register(defaults: ["token" : tokenString])
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        
+    }
+    
+    
+  
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> ()) {
+        
+        print("Message ID \(userInfo["data"]!)")
+         print("          ")
+        print(userInfo)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController: UIViewController = storyboard
+            .instantiateViewController(withIdentifier: "Main") as! ViewController
+        UserDefaults.standard.register(defaults: ["jsonDatas" : userInfo["data"]!])
+        //let defaults = UserDefaults.standard
+        //defaults.set(userInfo["data"] as! String, forKey: "jsonDatos")
+
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+    
+
+    
+    func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- WebBrowser/ViewController.swift
+++ WebBrowser/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+              // Do any additional setup after loading the view, typically from a nib.
+        self.title = "ViewController"
+        var url = URL(string: "https://www.samtech.cl/ANDROID/index.asp?v=1")
+        if UserDefaults.standard.string(forKey: "jsonDatas") != nil {
+            
+            let datos = UserDefaults.standard.string(forKey: "jsonDatas")?.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+            //datos = "&data={\"data\":\"aaaaaaaaaa\",\"mesa\":\"mesaaaa\"}".addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+            
+            url = URL(string: "https://www.samtech.cl/ANDROID/index.asp?v=2&token="+UserDefaults.standard.string(forKey: "token")!+"&data="+datos!)
+        }else if UserDefaults.standard.string(forKey: "token") != nil {
+            url = URL(string: "https://www.samtech.cl/ANDROID/index.asp?v=3&token="+UserDefaults.standard.string(forKey: "token")!)
+        }
+        let request = URLRequest(url: url!)
+        webView.scrollView.minimumZoomScale = 1.0;
+        webView.scrollView.maximumZoomScale = 1.0;
+        webView.loadRequest(request)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+ 
